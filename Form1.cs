bdb96b2cdf40e1e502ec8ceb705750746ab145db
Fernando Nicolás <fersanti2896@gmail.com>
2022-12-07 12:11:09 -0600

    Task.WhenAll en Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Concurrencia {
     public partial class Form1 : Form {
         public Form1() {
@@ -6,17 +8,45 @@ namespace Concurrencia {
 
         private async void button1_Click(object sender, EventArgs e) {
             pictureBox1.Visible = true;
-           
+
             /* Durmiendo el proceso */
             //Thread.Sleep(3000); Sincrono
-            await Task.Delay(3000);
-           
-            MessageBox.Show("Has ejecutado la acción");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var tareas = new List<Task>() {
+                procesamientoLargoA(),
+                procesamientoLargoB(),
+                procesamientoLargoC()
+            };
+
+            await Task.WhenAll(tareas);
+
+            sw.Stop();
+
+            var duracion = $"El programa se ejecutó en { sw.ElapsedMilliseconds / 1000.0 } segundos.";
+            Console.WriteLine($"{ duracion }");
             pictureBox1.Visible = false;
         }
 
         private void Form1_Load(object sender, EventArgs e) {
 
+        }
+
+        private async Task procesamientoLargoA() {
+            await Task.Delay(1000);
+            Console.WriteLine($"Procesamiento de A.");
+        }
+
+        private async Task procesamientoLargoB() {
+            await Task.Delay(1000);
+            Console.WriteLine($"Procesamiento de B.");
+        }
+
+        private async Task procesamientoLargoC() {
+            await Task.Delay(1000);
+            Console.WriteLine($"Procesamiento de C.");
         }
     }
 }
